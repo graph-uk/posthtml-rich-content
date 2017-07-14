5507c8c67882f8cd7e11517d68bc772a094e27a5
Vladimir Kuznetsov <vladimir.kuznetsov@graph.uk>
2017-07-14 19:18:35 +0500

    removed useless quote transformation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 exports.filterMarkup = require('./filterMarkup');
 exports.replaceImage = require('./replaceImage');
-exports.replaceQuote = require('./replaceQuote');
 exports.replaceVideo = require('./replaceVideo');
